8969ea60533c12e383cb9c7745add4afae5c592b
Ahmed Manai <manaii.ahmed9896@gmail.com>
2023-10-30 19:38:10 +0000

    add directive simple app

--- Hero-Project/src/app/app.component.ts
+++ Hero-Project/src/app/app.component.ts
@@ -6,32 +6,24 @@ import { AfterViewInit, Component, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  name!: string;
+  email!: string;
+  address!: string;
 
-  constructor(){
+  userArray: Array<any> = [];
 
+  onClick(){
+    this.userArray.push({
+      "name": this.name,
+      "email": this.email,
+      "address": this.address
+    });
+
+    console.log(this.userArray);
   }
 
-  objectArray: Array<any> = [
-    {id: 1, postTitle: 'Post 1'},
-    {id: 2, postTitle: 'Post 2'},
-    {id: 3, postTitle: 'Post 3'},
-    {id: 4, postTitle: 'Post 4'},
-    {id: 5, postTitle: 'Post 5'},
-  ];
-
-  stepForm:string = "Something Else";
-
-  onClick(status: any){
-    this.stepForm = status;
+  onDelete(index: any){
+    this.userArray.splice(index, 1);
   }
-
-  addNew(){
-    this.objectArray.push({id: 6, postTitle: 'Post 6'});
-  }
-
-  OnDelete(index: any){
-    this.objectArray.splice(index, 1);
-  }
-  isActive: boolean = true;
 }
 
