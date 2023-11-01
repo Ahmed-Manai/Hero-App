05ca183da251c9c060c36304e93318d772c5b8b4
Ahmed Manai <manaii.ahmed9896@gmail.com>
2023-11-01 17:25:28 +0000

    reactive form and nested form

--- Hero-Project/src/app/app.component.ts
+++ Hero-Project/src/app/app.component.ts
@@ -1,5 +1,6 @@
+import { TmplAstHoverDeferredTrigger } from '@angular/compiler';
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { FormControl, NgForm, NgModel } from '@angular/forms';
+import { FormGroup, FormControl, NgForm, NgModel, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,60 @@ import { FormControl, NgForm, NgModel } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  title: string  = "Angular Pipe";
 
-  onSubmit(fm: NgForm){
-    console.log(fm.value); 
+  title: string = "Angular Pipe";
+
+
+  form: any;
+
+  emailRegex: string = "[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$";
+  contactRegex: string = "[789][0-9][9]";
+
+  constructor() {
+    this.form = new FormGroup({
+      fullName: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      email: new FormControl('', [Validators.required,
+      //  Validators.pattern(this.emailRegex)
+      Validators.email
+      ]),
+
+      contactDetails: new FormGroup({
+        address: new FormControl('', [Validators.required]),
+        shippingAddress: new FormControl('', [Validators.required]),
+        contactNum: new FormControl('', [Validators.required, Validators.pattern(this.contactRegex)])
+      })
+    });
   }
 
-  getValue(fullName: NgModel ){
+  get ShippingAddress() {
+    return this.form.get('contactDetails.shippingAddress');
+  }
+  get ContactNum() {
+    return this.form.get('contactDetails.contactNum');
+  }
+  get Address() {
+    return this.form.get('contactDetails.address');
+  }
+  get Email() {
+    return this.form.get('email');
+  }
+
+
+  get fullname() {
+    return this.form.get('fullName');
+  }
+
+  onSubmitReactiveForm() {
+    console.log(this.form.value);
+  }
+
+
+
+  onSubmit(fm: NgForm) {
+    console.log(fm.value);
+  }
+
+  getValue(fullName: NgModel) {
     console.log(fullName);
   }
 }
